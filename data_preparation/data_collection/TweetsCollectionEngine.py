588f3449a797fc59435a0e7c114e6aabb6ae27ff
AlexTeng <79433869+AlexTengT@users.noreply.github.com>
2022-09-22 19:07:10 +1000

    update name

---
+++ data_preparation/data_collection/TweetsCollectionEngine.py
@@ -0,0 +1,79 @@
+import colorama.initialise
+import os
+import tweepy
+from BaseEngine import BaseEngine
+import config
+import logging
+
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+
+
+class TweetsCollectionEngine(BaseEngine):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_latest_tweet_on_db(self, userid):
+        _filter = {
+            'author_id': userid
+        }
+        _sort = list({
+                         'id': -1
+                     }.items())
+        _limit = 1
+
+        return list(self.get_col_raw_tweets().find(filter=_filter, sort=_sort, limit=_limit))[0]
+
+    def get_time_latest_tweet_on_db(self, userid):
+        """
+        This is used to query new tweets by API.
+        :param userid: the id of user
+        :return: the time of the latest tweet on database.
+        """
+        _filter = {
+            'author_id': userid
+        }
+        _project = {
+            'created_at': 1
+        }
+        _sort = list({
+                         'id': -1
+                     }.items())
+        _limit = 1
+
+        return list(self.get_col_raw_tweets().find(filter=_filter, sort=_sort, limit=_limit))[0]['created_at']
+
+    def get_all_users_on_db(self) -> list:
+        """
+        :return: A list, that contain the userid of all users on db. e.g. [12434, 23123, 34324]
+        """
+        # todo get distinct user_id from db
+        pass
+
+    def get_new_tweets_by_user(self, userid, _start_time=None):
+        """
+        :param userid:
+        :param _start_time:
+        :return: [<tweet1>, <tweet2>]
+        """
+        _client = self.get_api_client()
+        if _start_time is None:
+            _start_time = self.get_time_latest_tweet_on_db(userid)
+        _tweets_list = []
+        for status in tweepy.Paginator(_client.get_users_tweets, id=userid, max_results=5, limit=5,
+                                       tweet_fields=config.TWEET_FIELDS, start_time=_start_time):
+            for i in status.data:
+                _tweets_list.append(i)
+        return _tweets_list[1:]
+
+    def insert_new_tweets_by_user(self, userid, _start_time=None):
+        logging.info(f"Insert New Tweets [userid = {userid}]")
+        _insert_list = self.get_new_tweets_by_user(userid, _start_time)
+        _col = self.get_col_raw_tweets()
+        count = 0
+        for i in _insert_list:
+            count += 1
+            _col.insert_one(i)
+        logging.info(f"Length of updated_list: {len(_insert_list)}, insert {count} entries.")
+
+
---
+++ data_preparation/tests/test_TweetsCollectionEngine.py
@@ -0,0 +1,40 @@
+import datetime
+
+from BaseEngine import BaseEngine
+
+from data_collection.TweetsCollectionEngine import TweetsCollectionEngine
+from pprint import pprint
+
+tce = TweetsCollectionEngine()
+
+
+def test_get_latest_tweet_on_db():
+    s_name = "elonmusk"
+    userid = tce.get_user_id(screen_name=s_name)
+    res = tce.get_latest_tweet_on_db(userid)
+    print("-----")
+    pprint(res)
+
+
+def test_get_time_latest_tweet_on_db():
+    s_name = "elonmusk"
+    userid = tce.get_user_id(screen_name=s_name)
+    res = tce.get_time_latest_tweet_on_db(userid)
+    assert type(res) is datetime.datetime
+    pprint(res)
+
+
+def test_get_new_tweets_by_user():
+    s_name = "elonmusk"
+    userid = tce.get_user_id(screen_name=s_name)
+    res = tce.get_new_tweets_by_user(userid)
+    pprint(res)
+
+
+def test_get_new_tweets_by_user_2():
+    time = datetime.datetime.fromisoformat("2022-09-21T17:02:32.000+00:00")
+    print(time)
+    s_name = "elonmusk"
+    userid = tce.get_user_id(screen_name=s_name)
+    res = tce.get_new_tweets_by_user(userid, _start_time=time)
+    pprint(res)
