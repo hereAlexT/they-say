1e66a109f8234ac121637e5a7a4595a89591a24e
AlexTeng <79433869+AlexTengT@users.noreply.github.com>
2022-09-22 19:46:48 +1000

    conver_tweepy_object_to_dict

--- data_preparation/BaseEngine.py
+++ data_preparation/BaseEngine.py
@@ -28,3 +28,27 @@ class BaseEngine:
 
     def get_user_id(self, screen_name):
         return self.api_client.get_user(username=screen_name).data.id
+
+    @staticmethod
+    def convert_tweepy_object_to_dict(t):
+        refer_tweets = []
+        if t.referenced_tweets is not None:
+            for i in t.referenced_tweets:
+                refer_tweets.append(i.data)
+
+        _d = {'author_id': t.author_id,
+              'context_annotations': t.context_annotations,
+              'conversation_id': t.conversation_id,
+              'created_at': t.created_at,
+              'entities': t.entities,
+              'id': t.id,
+              'in_reply_to_user_id': t.in_reply_to_user_id,
+              'lang': t.lang,
+              'possibly_sensitive': t.possibly_sensitive,
+              'referenced_tweets': refer_tweets,
+              'reply_settings': t.reply_settings,
+              'source': t.source,
+              'text': t.text,
+              'data_from': "api"}
+
+        return _d
