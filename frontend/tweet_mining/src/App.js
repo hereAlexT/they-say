721001fc3a7e776cfdd7ee71999db2f0bdff787f
AlexTeng <79433869+AlexTengT@users.noreply.github.com>
2022-09-24 22:23:48 +1000

    update front end

--- frontend/tweet_mining/src/App.js
+++ frontend/tweet_mining/src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import WordFreqTable from './Components/WordFreqTable';
+import UserSearchBox from './Components/UserSearchBox';
+import './index.css';
+
+
+
 
 function App() {
   return (
     <div className="App">
+
       <h1>Tweet Mining</h1>
+      <p>Welcome to Tweet Mining, </p>
+      <UserSearchBox></UserSearchBox>
+      <Button variant="contained">Search</Button>
+      <WordFreqTable />
+
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
+
     </div>
   );
 }
+
+
+
 
 export default App;
