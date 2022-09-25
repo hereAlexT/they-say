98ee031cdb9b9d7568166e261dec519c061f1506
AlexTeng <79433869+AlexTengT@users.noreply.github.com>
2022-09-25 20:07:51 +1000

    searchbox basic

--- frontend/they-say/src/App.js
+++ frontend/they-say/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBox from './Components/SearchBox';
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>TheySay.app</h1>
+      <SearchBox />
+
     </div>
   );
 }
