fa3b6b8994fdf6a57851dd5ae33a165df7df0e41
AlexTeng <79433869+AlexTengT@users.noreply.github.com>
2022-09-28 15:08:29 +1000

    footer

---
+++ frontend/they-say-ts/they-say/src/Components/Footer.tsx
@@ -0,0 +1,37 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://theysay.tech/">
+                TheySay.tech
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+
+export default function Footer() {
+    return (
+        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+            <Typography variant="h6" align="center" gutterBottom>
+                -- END --
+            </Typography>
+            <Typography
+                variant="subtitle1"
+                align="center"
+                color="text.secondary"
+                component="p"
+            >
+                Hello World!
+            </Typography>
+            <Copyright />
+        </Box>
+    )
+}
